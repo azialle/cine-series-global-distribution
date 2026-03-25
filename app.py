382b03bf4faf1adfbb5e0e16f3bdc67f44d45497
Ellaiza Mae Lontok <emae.lontok@gmail.com>
2026-03-25 17:55:53 +0800

    Update visualizations to Plotly and improve chart styling

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import plotly.express as px
 from components import total_title_card, total_movie_card, total_tv_show_card, total_reach_card
 
 st.set_page_config(page_title="Netflix Dashboard", layout="wide")
@@ -11,7 +10,7 @@ with open("style.css") as file:
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv('data/processed/netflix_titles_cleaned.csv')
+    df = pd.read_csv("data/processed/netflix_titles_cleaned.csv")
     return df
 
 df = load_data()
@@ -19,37 +18,52 @@ df = load_data()
 st.title("Netflix Data Dashboard")
 
 total_val = len(df)
-total_movies = len(df[df['type'] == 'Movie'])
-total_tv_shows = len(df[df['type'] == 'TV Show'])
-
-total_countries = df['country'].nunique()
-min_year = df['release_year'].min()
-max_year = df['release_year'].max()
+total_movies = len(df[df["type"] == "Movie"])
+total_tv_shows = len(df[df["type"] == "TV Show"])
+total_countries = df["country"].nunique()
 
 
-col1, col2, col3, col4 = st.columns(4, gap='small') 
+card_col1, card_col2, card_col3, card_col4 = st.columns(4, gap="small") 
 
-with col1:
+with card_col1:
     st.markdown(total_title_card(total_val), unsafe_allow_html=True)
-with col2:
+with card_col2:
     st.markdown(total_movie_card(total_movies), unsafe_allow_html=True)
-with col3:
+with card_col3:
     st.markdown(total_tv_show_card(total_tv_shows), unsafe_allow_html=True)
-with col4:
+with card_col4:
     st.markdown(total_reach_card(total_countries), unsafe_allow_html=True)
 
 st.markdown("<br>", unsafe_allow_html=True)
 chart_col1, chart_col2 = st.columns(2)
 
 with chart_col1:
-    st.subheader("Content Added Per Year")
-    added_counts = df.groupby(['year_added', 'type']).size().reset_index(name='count')
+    content_metrics = df.groupby(["year_added", "type"]).size().reset_index(name="count")
     
-    fig1, ax1 = plt.subplots(figsize=(10, 6))
-    sns.barplot(data=added_counts, x='year_added', y='count', hue='type',  ax=ax1)
-    ax1.set_xlabel("Year Added")
-    ax1.set_ylabel("Count")
-    ax1.legend(title="Type")
-    fig1.patch.set_alpha(0)
-    ax1.set_facecolor((0,0,0,0))
-    st.pyplot(fig1)
+    fig1 = px.bar(data_frame=content_metrics,
+        x="year_added", 
+        y="count", 
+        color="type",
+        barmode="group",
+        color_discrete_map={'Movie': '#E50914', 'TV Show': '#0071eb'},
+        title="Content Added Per Year"
+    )
+    
+    min_yr = content_metrics["year_added"].min()
+    max_yr = content_metrics["year_added"].max()
+
+    fig1.update_layout(
+        xaxis_title="Year",
+        yaxis_title="Total Titles",
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+    
+        xaxis=dict(
+            tickmode="linear", 
+            tick0=min_yr,     
+            dtick=1,           
+            range=[min_yr - 0.5, max_yr + 0.5] 
+        )
+    )
+
+    st.plotly_chart(fig1, width="stretch")
