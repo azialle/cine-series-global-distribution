e8378a941c2347ad2e7621f47579d0b1fddcc1b3
Ellaiza Mae Lontok <emae.lontok@gmail.com>
2026-03-28 17:42:07 +0800

    Implement datetime conversion for date_added

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -4,5 +4,6 @@ import pandas as pd
 @st.cache_data
 def load_data():
     df = pd.read_csv("data/processed/netflix_titles_cleaned.csv")
+    df["date_added"] = pd.to_datetime(df["date_added"].str.strip(), errors="coerce")
     df["title"] = df["title"].str.title()
     return df
