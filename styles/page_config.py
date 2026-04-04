d1fb1979eebf09aeef920ef5ac6aac625b902131
Ellaiza Mae Lontok <emae.lontok@gmail.com>
2026-04-04 21:47:11 +0800

    Refactor old CSS and implement new layout and header styles also added page_config.py for more organized page configuration

---
+++ styles/page_config.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def page_config():
+    st.set_page_config(page_title="Netflix Dashboard", layout="wide", page_icon="🎬")
+    with open("styles/style.css", encoding="UTF-8") as file:
+        st.markdown(f"<style>{file.read()}</style>", unsafe_allow_html=True)
